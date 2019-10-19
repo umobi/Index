da8f243598756cbd56548f44dd587d1916fc69d2
brennobemoura <brennobemoura@outlook.com>
2019-10-19 19:55:35 -0300

    UIStackView with reload cells using IndexBindable

--- Container/IndexBindable+UIContainer.swift
+++ Container/IndexBindable+UIContainer.swift
@@ -19,3 +19,37 @@ public extension UIContainer where View: IndexBindable {
         }
     }
 }
+
+public extension UIStackView {
+    @discardableResult
+    func reloadArrangedSubviews<C: UIContainer, E>(_ container: C.Type, with elements: [E], in parentView: C.ParentView!) -> [(C, E)] where C: UIView, C.View: IndexBindable, E == C.View.Model.Index {
+        var containers: [C] = self.arrangedSubviews.compactMap {
+            guard let container = $0 as? C else {
+                $0.removeFromSuperview()
+                return nil
+            }
+
+            return container
+        }
+
+        if containers.count > elements.count {
+            containers[elements.count..<containers.count].forEach {
+                $0.removeContainer()
+            }
+            containers = Array(containers[0..<elements.count])
+
+        } else if elements.count > containers.count {
+            containers = containers + (containers.count..<elements.count).map { _ in
+                return C.init(in: parentView, loadHandler: nil)
+            }
+        }
+
+        return elements.enumerated().map {
+            containers[$0.offset].view.configure($0.element)
+            if containers[$0.offset].superview == nil {
+                self.addArrangedSubview(containers[$0.offset])
+            }
+            return (containers[$0.offset], $0.element)
+        }
+    }
+}
